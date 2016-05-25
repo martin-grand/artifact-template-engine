2a94732009c422a3ba269d38b8129a5d96138b14
Martin Grand <marton.nagy@martingrand.com>
2016-05-25 14:55:59 +0200

    Update engine.js

--- engine.js
+++ engine.js
@@ -75,7 +75,7 @@
 				return fn.call(context, {}, context, false, helpers, partials);
 			}
 		} catch (e) {
-			console.error('Ouch', e, getFnStringFromTemplateString(_templateString));
+			console.error(e, getFnStringFromTemplateString(_templateString));
 		}
 		
 	};
